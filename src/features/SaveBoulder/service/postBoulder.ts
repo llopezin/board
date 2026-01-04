c312683faa310c23afdf2a9590168fca8a41ad89
Laura <lauralopezinfantes@gmail.com>
2026-01-04 17:45:29 +0100

    fix: revalidate after boulder addition

--- src/features/SaveBoulder/service/postBoulder.ts
+++ src/features/SaveBoulder/service/postBoulder.ts
@@ -8,8 +8,10 @@ export default async function postBoulder(boulder: Omit<BoulderListItemDto, "id"
     try {
         const collection = db.collection(collectionNames.boulder);
 
+        const res = await collection.add(boulder);
         revalidateTag(cacheTags.boulderList, 'max');
-        return collection.add(boulder);
+
+        return res;
     } catch (error) {
         console.warn("error posting boulder: ", error);
 
