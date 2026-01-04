e4d6c96166b6c990af661845efdb49cf642b23ee
Laura <lauralopezinfantes@gmail.com>
2026-01-04 17:15:45 +0100

    fix: correct cache tag name

--- src/features/SaveBoulder/service/postBoulder.ts
+++ src/features/SaveBoulder/service/postBoulder.ts
@@ -2,12 +2,13 @@ import { BoulderListItemDto } from "@/domain/dtos/BoulderListItem.dto";
 import { db } from "@/lib/firebase/client";
 import { collectionNames } from "@/constants/collection-names";
 import { revalidateTag } from "next/cache";
+import { cacheTags } from "@/constants/cache-tags";
 
 export default async function postBoulder(boulder: Omit<BoulderListItemDto, "id">) {
     try {
         const collection = db.collection(collectionNames.boulder);
 
-        revalidateTag(collectionNames.boulder, 'max');
+        revalidateTag(cacheTags.boulderList, 'max');
         return collection.add(boulder);
     } catch (error) {
         console.warn("error posting boulder: ", error);
