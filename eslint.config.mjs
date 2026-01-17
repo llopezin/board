c8f6eb22197af36c3fd633dc4a8e2973d4579517
Laura <lauralopezinfantes@gmail.com>
2026-01-17 15:00:43 +0100

    chore: config to remove unused imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,29 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+import unusedImports from "eslint-plugin-unused-imports";
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          "vars": "all",
+          "varsIgnorePattern": "^_",
+          "args": "after-used",
+          "argsIgnorePattern": "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
