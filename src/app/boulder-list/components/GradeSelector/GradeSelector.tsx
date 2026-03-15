f44251af4e725847d3af6cff5a6627ab94f583e4
Laura <lauralopezinfantes@gmail.com>
2026-03-15 19:32:58 +0100

    fix: grade selector padding

--- src/app/boulder-list/components/GradeSelector/GradeSelector.tsx
+++ src/app/boulder-list/components/GradeSelector/GradeSelector.tsx
@@ -18,16 +18,16 @@ function GradeSelector({ setGrades, grades }: GradeSelectorProps) {
     }
 
     return (
-        <div className="px-4 pb-4 text-white">
+        <div className="pb-4 text-white">
             <div className="flex items-center gap-2 mb-2">
-                <h2 className="text-white ">Filter by grade:</h2>
+                <h2 className="px-4 text-white ">Filter by grade:</h2>
                 {!!grades?.length && <button className="bg-white text-black px-2 rounded-full" onClick={() => setGrades(undefined)}>Clear</button>}
             </div>
             <form className="overflow-x-auto no-scrollbar relative" action="/boulder-list" >
-                <fieldset className="flex gap-2">
+                <fieldset className="flex gap-2 pr-4">
                     <legend className="sr-only">Filter by grade</legend>
-                    {boulderGrades.map((grade) => (
-                        <div key={grade}>
+                    {boulderGrades.map((grade, i) => (
+                        <div className={i === 0 ? 'pl-4' : ''} key={grade}>
                             <input className="peer sr-only" onChange={handleChange} type="checkbox" id={grade} name="grade" value={grade} checked={!!grades && grades.includes(grade)} />
                             <label className="border-2 border-purple-400 peer-checked:bg-purple-400 px-3 py-1 block rounded-full text-white font-bold" htmlFor={grade}>
                                 <span>{grade}</span>
